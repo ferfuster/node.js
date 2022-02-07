83952421b8aa2d815b196ccabda130bc80832d01
ferfuster <ferfuster2010@hotmail.com>
2022-02-07 18:51:11 -0300

    callBack

--- primerApp/app.js
+++ primerApp/app.js
@@ -16,3 +16,31 @@ console.log(edad);
 
 let edad1 = anios.map(el=> 2019-el);
 console.log(edad1);
+
+
+//funciones CallBack
+
+function Mensaje (callback){
+    console.log('Msj antes del callback');
+    callback();
+}
+function Saludo (){
+    console.log('Mjs despues del call');
+}
+
+Mensaje(Saludo);
+
+/*la funcion Mensaje recibe como parametro a la funcion Saludo */
+
+function Sumar (num1, num2, callback){
+    let resultado=num1+num2;
+    callback(resultado);
+}
+function Resultado(res){
+    console.log(res);
+}
+Sumar(5, 8, Resultado);
+
+/*setTimeout es una funcion callback propia de Js */
+
+setTimeout(()=> console.log('se ejecuta luego de 3 segundos'),3000);
