102088f481981769ccc5817fceade4d54ffbdcf4
ferfuster <ferfuster2010@hotmail.com>
2022-02-18 11:56:19 -0300

    Modulo Events

--- modulos/app.js
+++ modulos/app.js
@@ -6,6 +6,10 @@ console.log(objetoPath);
 console.log("Se muestra solo en nombre:  "+ objetoPath.name);*/
 
 
+
+//-----------Modulo OS
+
+/*
 const os = require('os');
 
 var memoriaLibre= os.freemem();
@@ -13,3 +17,16 @@ var memoriaTotal= os.totalmem();
 
 console.log(`memoria Libre:  ${memoriaLibre}`);
 console.log(`memoria Total: ${memoriaTotal}`);
+*/
+
+//----modulo File System
+
+const fs = require ('fs');
+
+//const archivos = fs.readdirSync('./');
+//console.log(archivos);
+
+fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Resultado', files);
+});
