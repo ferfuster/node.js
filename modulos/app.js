70b10a6f29a36beb4c8730a9af79510b7966f902
ferfuster <ferfuster2010@hotmail.com>
2022-02-18 11:46:27 -0300

    Modulo File System

--- modulos/app.js
+++ modulos/app.js
@@ -1,6 +1,15 @@
-const path = require ('path');
+/*const path = require ('path');
 
 const objetoPath = path.parse(__filename);
 
 console.log(objetoPath);
-console.log("Se muestra solo en nombre:  "+ objetoPath.name);
+console.log("Se muestra solo en nombre:  "+ objetoPath.name);*/
+
+
+const os = require('os');
+
+var memoriaLibre= os.freemem();
+var memoriaTotal= os.totalmem();
+
+console.log(`memoria Libre:  ${memoriaLibre}`);
+console.log(`memoria Total: ${memoriaTotal}`);
