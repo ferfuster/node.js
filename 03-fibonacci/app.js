f044fc58cc69a6b5c7acaf00aeb419e4bef2c8e5
ferfuster <ferfuster2010@hotmail.com>
2022-03-11 15:57:11 -0300

    crear archivo .txt

--- 03-fibonacci/app.js
+++ 03-fibonacci/app.js
@@ -1,12 +1,25 @@
 //Serie de Fibonacci
 
-let fibo1 = 1;
-let fibo2 = 2;
+const fs = require ('fs');
 
-console.log(`${fibo1}`);
+
+let fibo1 = 1;
+let fibo2 = 1;
+let serie = '';
+
+serie += `${fibo1}\n`;
+//console.log(`${fibo1}`);
 
 for(let i= 2;i<=7 ; i++){
-    console.log(`${fibo2}`);
+    //console.log(`${fibo2}`);
+    serie+=`${fibo2}\t`;
     fibo2=fibo1+fibo2;
     fibo1=fibo2-fibo1;
 }
+
+
+fs.writeFile('fibonacci.txt', serie, (err)=>{
+    if (err) throw err;
+    console.log('El archivo fue creado con éxito.');
+}
+);
