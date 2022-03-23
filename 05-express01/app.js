8c94b417039c21a5d8cb0465eced09f11634b2fc
ferfuster <ferfuster2010@hotmail.com>
2022-03-23 16:02:49 -0300

    API/res

--- 05-express01/app.js
+++ 05-express01/app.js
@@ -1,6 +1,11 @@
 const express= require('express');
 const app = express();
 
+const usuarios =[
+    {id:1, nombre:"José"},
+    {id:2, nombre:"fede"},
+    {id:3, nombre:"andres"}
+];
 
 app.get('/', (req, res)=>{
     res.send('Express desde el inicio.');
@@ -11,9 +16,13 @@ app.get('/api/usuarios',(req, res)=>{
     res.send(['grover','luis','ana']);
 })
 
+app.get('/api/usuarios/:id',(req, res)=>{
+    let usuario = usuarios.find(u =>u.id === parseInt(req.params.id));
+    if(!usuario)res.status(404).send('El usuario no fue encontrado...');
+    res.send(usuario);
+});
+
 const port = process.env.PORT || 3000;
-
-
 app.listen(port,()=>{
     console.log(`puerto ${port} iniciado.....`);
 })
