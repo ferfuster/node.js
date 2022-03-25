0f5f74875210a82f860df55e930fb23829a0bcd9
ferfuster <ferfuster2010@hotmail.com>
2022-03-24 22:11:48 -0300

    Metodo Delete

--- 05-express01/app.js
+++ 05-express01/app.js
@@ -21,7 +21,8 @@ app.get('/api/usuarios',(req, res)=>{
 });
 
 app.get('/api/usuarios/:id',(req, res)=>{
-    let usuario = usuarios.find(u =>u.id === parseInt(req.params.id));
+   // let usuario = usuarios.find(u =>u.id === parseInt(req.params.id));
+   let usuario = existeUsuario(req.params.id);
     if(!usuario) res.status(404).send('El usuario no fue encontrado...');
     res.send(usuario);
 });
@@ -30,7 +31,7 @@ app.post('/api/usuarios', (req, res)=>{
     const schema=Joi.object({
         nombre: Joi.string().min(3).required()
     });
-    const {error, value}=schema.validate({nombre: req.body.nombre});
+    const {error, value}=validarUsuario(req.body.nombre);
 
     if(!error){
     const usuario={
@@ -47,25 +48,49 @@ app.post('/api/usuarios', (req, res)=>{
 
 app.put('/api/usuarios/:id',(req, res)=>{
     //encontrar si existe el objeto
-    let usuario=usuarios.find(u=>u.id===parseInt(req.params.id));
-    if(!usuario) res.status(404).send('El usuario no fue encontrado');
+    //let usuario=usuarios.find(u=>u.id===parseInt(req.params.id));
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) {
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }
 
-const schema=Joi.object({
-    nombre: Joi.string().min(3).required()
-});
-const {error, value}= schema.validate({nombre: req.body.nombre});
-if(error){
-    const mensaje = error.details[0].message;
-    res.status(400).send(mensaje);
-    return;
-}
+    const {error, value}= validarUsuario(req.body.nombre);
+    if(error){
+        const mensaje = error.details[0].message;
+        res.status(400).send(mensaje);
+        return;
+    }
 usuario.nombre=value.nombre;
 res.send(usuario);
-
 });
 
+app.delete('/api/usuarios/:id', (req, res) =>{
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) {
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }
+
+    const index =usuarios.indexOf(usuario);
+    usuarios.splice(index,1);
+
+    res.send(usuario);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`puerto ${port} iniciado.....`);
 });
+
+function existeUsuario(id){
+    return(usuarios.find(u=>u.id===parseInt(id)));
+}
+
+
+function validarUsuario(nom){
+    const schema=Joi.object({
+        nombre: Joi.string().min(3).required()
+    });
+    return(schema.validate({nombre: nom}))
+}
